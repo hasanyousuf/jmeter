a5ad811c15ecabd044bee35a1f8ff31d15245510
hasany@gmail.com <Hasany@gmail.com>
2018-08-29 23:10:10 +0200

    Ready for jenkins

--- runTest.sh
+++ runTest.sh
@@ -1,3 +1,3 @@
 rm -rf project/repo/*
-sh apache-jmeter-4.0/bin/jmeter -n -t project/server.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
+sh apache-jmeter-4.0/bin/jmeter -n -t project/serverTry.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
 open ./project/repo/Reports/
