3340d3fd4ed3115f0f722621f27cffe39dd9e361
hasany@gmail.com <Hasany@gmail.com>
2018-08-29 23:29:59 +0200

    Removed unused files

--- runTest1.sh
+++
@@ -1,3 +0,0 @@
-rm -rf project/repo/*
-sh apache-jmeter-4.0/bin/jmeter Jjmeter.save.saveservice.output_format=xml -Jjmeter.save.saveservice.output_format=csv -n -t project/server.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
-open ./project/repo/Reports/
