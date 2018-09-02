12f1058a9c2a654d7fa23e011b8a117efc3fe79a
Hasan <hasany@gmail.com>
2018-09-02 18:36:18 +0200

    Debug parameters

--- runTest.sh
+++ runTest.sh
@@ -6,6 +6,7 @@ echo "server is: " $4
 echo "port is: " $5
 echo "http is: " $6
 
+
 rm -rf project/repo/*
-sh apache-jmeter-4.0/bin/jmeter -JUser="$1" -JRampUp="$2" -JLoop="$3" -JServer="$4" -JPort="$5" -JHttp="$6" -n -t project/cake.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
+echo  apache-jmeter-4.0/bin/jmeter -JUser="$1" -JRampUp="$2" -JLoop="$3" -JServer="$4" -JPort="$5" -JHttp="$6" -n -t project/cake.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
 #open ./project/repo/Reports/
