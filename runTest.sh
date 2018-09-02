8160ec1e6c1b7d7f5734a2ae34fd9ad34a34588b
Hasan <hasany@gmail.com>
2018-09-02 19:07:56 +0200

    Added more parameters & removed old jmx file

--- runTest.sh
+++ runTest.sh
@@ -8,5 +8,5 @@ echo "http is: " $6
 
 
 rm -rf project/repo/*
-sh  apache-jmeter-4.0/bin/jmeter -JUser="$1" -JRampUp="$2" -JLoop="$3" -JServer="$4" -JPort="$5" -JHttp="$6" -n -t project/cake.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
+sh  apache-jmeter-4.0/bin/jmeter -JUser="$1" -JRampUp="$2" -JLoop="$3" -JServer="$4" -JPort="$5" -JHttp="$6" -JResponseTime="$7" -n -t project/cake.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
 #open ./project/repo/Reports/
