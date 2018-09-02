356e9381d1aaf7d82a9feeba0615547e4cf4ef1c
hasany@gmail.com <Hasany@gmail.com>
2018-09-02 15:57:15 +0200

    Added parameters to be use with CI tools

--- runTest.sh
+++ runTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf project/repo/*
-sh apache-jmeter-4.0/bin/jmeter -n -t project/cake.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
-open ./project/repo/Reports/
+sh apache-jmeter-4.0/bin/jmeter -JUser=25 -JRampUp=1 -JLoop=10 -n -t project/cake.jmx -l project/repo/HTML/cake.csv -e -o project/repo/Reports/
+#open ./project/repo/Reports/
